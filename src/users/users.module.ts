f2cfcaa288d3beb23bf5955ad9ce6dd19fa5ca7c
Mohammad Moiz Ali <muhammad.moiz@takafulbazaar.pk>
2023-07-10 16:44:33 +0500

    add entity to report module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Report } from 'src/reports/report.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Report])],
   controllers: [UsersController],
   providers: [UsersService]
 })
