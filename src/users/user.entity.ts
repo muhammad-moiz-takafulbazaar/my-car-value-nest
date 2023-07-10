e42b6d6b52044858a87db76ab3a2a5c7b0393ea4
Mohammad Moiz Ali <muhammad.moiz@takafulbazaar.pk>
2023-07-10 16:31:10 +0500

    create user entity

---
+++ src/users/user.entity.ts
@@ -0,0 +1,13 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn()
+    id: Number;
+
+    @Column()
+    email: string;
+
+    @Column()
+    password: string;
+}
