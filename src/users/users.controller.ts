8b50ce6aad02fdace4fdd4944e7c92cb4f940808
Mohammad Moiz Ali <muhammad.moiz@takafulbazaar.pk>
2023-07-11 11:50:55 +0500

    add dto in the in user controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,9 +1,10 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user-dto';
 
 @Controller('auth')
 export class UsersController {
     @Post('/signup')
-    createUser() {
+    createUser(@Body() body: CreateUserDto) {
         
     }
 }
