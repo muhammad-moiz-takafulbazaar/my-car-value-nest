7dd5ab6b3d76303dc452cb39aea84afa8741466d
Mohammad Moiz Ali <muhammad.moiz@takafulbazaar.pk>
2023-07-11 14:40:56 +0500

    create create function in usersService file

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -5,5 +5,11 @@ import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class UsersService {
-    constructor(@InjectRepository(User) repo: Repository<User>) {}
+    constructor(@InjectRepository(User) private repo: Repository<User>) {}
+
+    create(email: string, password: string) {
+        const user = this.repo.create({ email, password })
+
+        return this.repo.save(user)
+    }
 }
