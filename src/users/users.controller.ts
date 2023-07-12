76725719a616f302c2c84af87b6181ab89677ed9
Mohammad Moiz Ali <muhammad.moiz@takafulbazaar.pk>
2023-07-12 11:09:22 +0500

    create removeUser function in controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Query } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user-dto';
 import { UsersService } from './users.service';
 
@@ -21,5 +21,8 @@ export class UsersController {
         return this.userService.find(email)
     }
 
-    
+    @Delete('/:id')
+    removeUser(@Param('id') id: string) {
+        this.userService.remove(parseInt(id))
+    }
 }
