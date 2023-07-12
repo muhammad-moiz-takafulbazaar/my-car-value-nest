99c05b209ca214ac4ff113f5f0f7bfc308a3f202
Mohammad Moiz Ali <muhammad.moiz@takafulbazaar.pk>
2023-07-12 11:00:43 +0500

    create findUser function in user controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,13 +1,18 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user-dto';
 import { UsersService } from './users.service';
 
 @Controller('auth')
 export class UsersController {
-    constructor(private userService: UsersService) {}
-    
+    constructor(private userService: UsersService) { }
+
     @Post('/signup')
     createUser(@Body() body: CreateUserDto) {
         this.userService.create(body.email, body.password)
+    }
+
+    @Get('/:id')
+    findUser(@Param('id') id: string) {
+        return this.userService.findOne(parseInt(id));
     }
 }
